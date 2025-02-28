3cacc4efa6dd5e80409c8f1779cdeada5dcb2fa4
adjohari <adjohari25@gmail.com>
2025-02-28 19:21:17 +0000

    updated Dockerfile with indexing

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 # from flask_session import Session
 import logging
 
-from api.users import users_blueprint
+# from api.users import users_blueprint
 from api.search import search_blueprint
 from api.autocomplete import autocomplete_blueprint, init_trie
 from global_path import get_relative_path
@@ -19,7 +19,7 @@ CORS(app, supports_credentials=True)
 
 init_trie(get_relative_path("api", "ingredients.json"))
 
-app.register_blueprint(users_blueprint, url_prefix='/api')
+# app.register_blueprint(users_blueprint, url_prefix='/api')
 app.register_blueprint(search_blueprint, url_prefix='/api')
 app.register_blueprint(autocomplete_blueprint, url_prefix='/api')
 
