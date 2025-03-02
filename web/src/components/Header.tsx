ff966f237e5d3f39bc4e01ce94445931b6fc0444
adjohari <adjohari25@gmail.com>
2025-03-02 10:13:59 +0000

    removed user authentication feature

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,23 +1,10 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { useNavigate } from 'react-router-dom';
 import { useSearch } from '../store/SearchContext'; // <-- ADDED
-import { useUser } from '../store/UserContext';
-import { GoogleIcon } from './icons';
 
 const Header = () => {
   const navigate = useNavigate();
-  const { user, signIn, signOut } = useUser();
   const { setSearchState } = useSearch(); // <-- ADDED
 
-  const googleLogin = async () => {
-    await signIn();
-  };
-
-  const handleSignOut = async () => {
-    await signOut();
-  };
-
-  // NEW: Clear search state and navigate to initial view
   const handleLogoClick = () => {
     setSearchState((prev) => ({
       ...prev,
@@ -36,43 +23,12 @@ const Header = () => {
   return (
     <header className="bg-gray-900 bg-opacity-90 shadow-md text-white">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        {/* UPDATED onClick to call handleLogoClick */}
         <h1
           className="text-3xl font-semibold cursor-pointer"
           onClick={handleLogoClick}
         >
           Dishcovery
         </h1>
-        <Menu as="div" className="relative">
-          <MenuButton className="text-sm font-medium focus:outline-none">
-            {user ? user.name : 'Profile / Sign In'}
-          </MenuButton>
-          <MenuItems className="absolute right-0 mt-2 bg-white shadow-lg rounded-md py-1 w-48 text-gray-900">
-            {!user ? (
-              <MenuItem>
-                {({ active }) => (
-                  <button
-                    className={`${active ? 'bg-gray-100' : ''} flex items-center px-4 py-2 w-full text-sm`}
-                    onClick={() => googleLogin()}
-                  >
-                    <GoogleIcon className="mr-2 h-5 w-5" /> Sign in with Google
-                  </button>
-                )}
-              </MenuItem>
-            ) : (
-              <MenuItem>
-                {({ active }) => (
-                  <button
-                    className={`${active ? 'bg-gray-100' : ''} flex items-center px-4 py-2 w-full text-sm`}
-                    onClick={handleSignOut}
-                  >
-                    Sign Out
-                  </button>
-                )}
-              </MenuItem>
-            )}
-          </MenuItems>
-        </Menu>
       </div>
     </header>
   );
